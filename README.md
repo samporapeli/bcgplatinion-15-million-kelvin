a96611c4f74080e09973de933e1f4d7288a8cb1b
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2022-10-29 11:23:38 +0300

    Add RPi autostart setup

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ## Datacenter simulation
 ## requirements and setup (datacenter-worker)
 Clone the repository into the RPi and run `setup_rpi.sh`
+It's necessary to have an autologin in place (use `raspi-config`), preferably in the terminal.
 ## setup (datacenter-instructor)
 Follow the `README.md` of the directory
--- datacenter-worker/setup_rpi.sh
+++ datacenter-worker/setup_rpi.sh
@@ -2,5 +2,12 @@
 
 set -e
 
+echo 'PATH=$PATH:$HOME/bcgplatinion-15-million-kelvin/datacenter-worker/
+# Simple autostart
+TMUX_PROCESS="$(pgrep tmux)"
+if [[ -z "$TMUX_PROCESS" ]]; then
+  tmux new-session "leds_off.py && /home/admin/bcgplatinion-15-million-kelvin/datacenter-worker/main.py" \; detach
+fi
+' >> $HOME/.bashrc
 sudo apt install -y python3-rpi.gpio
 sudo pip3 install requests
